7b4eab18a14a1dc99ca76ef97948db62949ef2cc
Giovanni Benussi <giovanni.benussi@usach.cl>
2014-12-04 09:08:12 -0300

    Comenté constructor EdgeServer

--- edgeserver/EdgeServer.h
+++ edgeserver/EdgeServer.h
@@ -23,25 +23,33 @@ private:
     LRU *ANSWERS;
     vector<double> cache_hits_vector;
 public:
-    EdgeServer(const string &name, int id, int type, int WSECACHESIZE) : Node(name, id, type)
+/**
+  *
+  * - WSECACHESIZE: Tamaño del cache del edge server (definido en Constants.h)
+  *
+  **/
+EdgeServer(const string &name, int id, int type, int WSECACHESIZE) : Node(name, id, type)
+{
+    idle_time = 0;
+    busy_time = 0;
+    received_querys_from_count = new unsigned int[ NUM_CLIENTS ];
+    for (int i = 0; i < NUM_CLIENTS; ++i)
     {
-        idle_time = 0;
-        busy_time = 0;
-        received_querys_from_count = new unsigned int[ NUM_CLIENTS ];
-        for (int i = 0; i < NUM_CLIENTS; ++i)
-        {
-            received_querys_from_count[ i ] = 0;
-        }
-
-        this->received_queries_by_clients                  = 0;
-        this->cache_hits_received_queries_by_clients       = 0;
-        this->cache_miss_received_queries_by_clients       = 0;
-        this->processed_queries                            = 0;
-
-        int * cacheSize = new int(WSECACHESIZE);
-        ANSWERS = new LRU(cacheSize);
-        cache_hits_vector.push_back(0);
+        received_querys_from_count[ i ] = 0;
     }
+
+    this->received_queries_by_clients                  = 0;
+    this->cache_hits_received_queries_by_clients       = 0;
+    this->cache_miss_received_queries_by_clients       = 0;
+    this->processed_queries                            = 0;
+
+    int * cacheSize = new int(WSECACHESIZE);
+    ANSWERS = new LRU(cacheSize);
+    // Se añade un valor de 0, ya que si no, en la primera iteración, al intentar obtener el elemento 0
+    // nos saldríamos del rango del vector, y tendríamos que comprobar siempre que el tamaño sea
+    // mayor a cero, en cambio, con esto nos ahorramos la comprobación e indicamos que en t = 0, no hay cache hits
+    cache_hits_vector.push_back(0);
+}
     ~EdgeServer()
     {
         delete ANSWERS;
